3bff2856d7c5ed73ba4c26dcb605338021609dfe
snwolak <snwolak@outlook.com>
2018-09-23 11:35:57 +0200

    add
    - verifing if incoming post to delete has the same author
    post in database has

--- functions/deletePost.js
+++ functions/deletePost.js
@@ -4,13 +4,31 @@ const requestIp = require('request-ip');
 const app = (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   const ip = requestIp.getClientIp(req);
-  if (ip === '') {
-    db.collection("posts").doc(req.body.permlink).delete().then(() => {
-      console.log("Document successfully deleted!");
-      return void 0;
-  }).catch((error) => {
-      console.error("Error removing document: ", error);
-  });
+  if (ip === "") {
+    const docRef = db.collection('posts').doc(req.body.permlink);
+    docRef
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        const post = doc.data()
+        if(post.author === req.body.author) {
+          db.collection("posts").doc(req.body.permlink).delete();
+          return void 0;
+        } else {
+          console.log("Unauthorized", req.body.author, req.body.permlink)
+          return void 0;
+        }
+        
+      } else {
+        console.log('No such document', req.body.permlink);
+        res.end();
+        return void 0;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    
     res.status(200).send('OK')
     res.end()
     return void 0;
