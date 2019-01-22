7dc71d3ca260294863489ed9c0581ebb7229563e
snwolak <snwolak@outlook.com>
2019-01-22 17:42:17 +0100

    add steem profile creation and add more info to profile

--- functions/createProfileSteem.js
+++ functions/createProfileSteem.js
@@ -6,8 +6,9 @@ const blog = require('./blog')
 const app = (req, res) => {
   cors(req, res, () => {  
   const data = req.body
-  const docRef = db.collection('users').doc(req.body.displayName);
+  const docRef = db.collection('users').doc(req.body.steemAccount);
   const checkToken = defaultApp.app.auth().verifyIdToken(data.token)
+  
   .then((decodedToken) => {
     const uid = decodedToken.uid;
     if(uid === data.uid) {
@@ -18,20 +19,22 @@ const app = (req, res) => {
   }).catch((error) => {
     // Handle error
   });
-  if(checkToken)
-  docRef.set({ isNSFWAllowed: false, owner: req.body.uid, platform: "email" }).then(res => { return res})
-  .catch((error) => {
-    console.error("Error writing document: ", error);
-  });
-  docRef.collection('blog').doc('layout').set(blog.layout({displayName: req.body.displayName}), { merge: true}).then(res => { return res})
-  .catch((error) => {
-    console.error("Error writing document: ", error);
-  });
-  res.status(200).send(req.body);
-  res.end()
+  if(checkToken) {
+    console.log(data)
+    docRef.set({ isNSFWAllowed: false, owner: req.body.steemAccount, platform: "steem" }).then(res => { return res})
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+    docRef.collection('blog').doc('layout').set(data.blog, { merge: true}).then(res => { return res})
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+    res.status(200).send(req.body);
+    res.end()
+  }
+  
   })
 };
-
 module.exports = {
   app
 }
--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const reciveToken = require('./reciveToken')
 const syncPosts = require('./syncPosts')
 const deletePost = require('./deletePost')
 const createProfile = require('./createProfile')
+const createProfileSteem = require('./createProfileSteem')
 const saveComment = require('./saveComment')
 const editTheme = require('./editTheme')
 
@@ -12,5 +13,6 @@ exports.syncPosts = functions.https.onRequest(syncPosts.app);
 exports.reciveToken = functions.https.onRequest(reciveToken.app);
 exports.deletePost = functions.https.onRequest(deletePost.app);
 exports.createProfile = functions.https.onRequest(createProfile.app);
+exports.createProfileSteem = functions.https.onRequest(createProfileSteem.app);
 exports.saveComment = functions.https.onRequest(saveComment.app)
 exports.editTheme = functions.https.onRequest(editTheme.app)
